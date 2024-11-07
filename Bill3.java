06c9ace178902ca2666fed5ac3242d28fe3549f1
University <inonuni@ilahyany-mbp.local>
2024-11-07 19:16:10 +0200

    Completed All 5 Tasks

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,11 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String nameOfDiner1 = args[0];
+	    String nameOfDiner2 = args[1];
+		String nameOfDiner3 = args[2];
+		double restaurantBill = Integer.parseInt(args[3]);
+
+		System.out.println("Dear " + nameOfDiner1 + ", " + nameOfDiner2 + ", and " + nameOfDiner3 + ": pay " + Math.ceil(restaurantBill/3) + " Shekels each.");
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,12 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int numberGiven = Integer.parseInt(args[0]);
+		int numOfHundreds, numOfTens, numOfOnes; 
+		numOfHundreds = (numberGiven/100);
+		numOfTens = (numberGiven - (numOfHundreds*100))/10;
+		numOfOnes = (numberGiven - (numOfHundreds*100 + numOfTens*10));
+		System.out.println(numOfHundreds + " hundreds, " + numOfTens + " tens, and " + numOfOnes + " ones.");
+
 	}
 }
